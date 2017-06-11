34dac2a6919288744e2316adb129f04de47c2e23
598846720@qq.com <598846720@qq.com>
2017-06-11 17:18:45 +0800

    优化app路由；增加数据请求为缓存数据和实时数据，增加bootstrap资源

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var ejs = require('ejs');
 var approuter=express.Router;
 var routes = require('./routes/index');
-routes(app);
 app.set('port', (process.env.PORT ||8080));
 app.use(express.static('public'));
 app.use(express.static('views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
-app.use(routes);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/', express.Router());
+app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 
 // Additional middleware which will set headers that we need on each request.
@@ -29,6 +30,7 @@ app.use(function(req, res, next) {
     res.setHeader('Cache-Control', 'no-cache');
     next();
 });
+routes(app);
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,45 @@
 'use strict';
-var request = require('sync-request');
-var cheerio = require('cheerio');
-var fs = require('fs');
-module.exports = function (app) {
-    app.get('/optionsUrl',function (req,res) {
-        console.log("optionsUrl");
-        var url='http://top.zhaopin.com/Home/Top50?id=HotJobTop&city=749&time=2';
-        var html = request('GET', url).getBody().toString();
-        var $ = cheerio.load(html);
-        var opt=[];
-        var outputFilePath='./public/json/options.json';
-        $('#industry_10').children('option').each(function () {
-            var json={};
-            json.val=$(this).val();
-            json.text=$(this).text();
-            opt.push(json);
-        });
-        var filestr={
-            data:opt,
-            name:'所有行业选项'
-        };
-        fs.writeFileSync(outputFilePath,JSON.stringify(filestr));
+let request = require('sync-request');
+let cheerio = require('cheerio');
+let fs = require('fs');
+module.exports = function(app) {
+    app.post('/optionsUrl',function (req,res){
+        let type=req.body.type;
+        let outputFilePath='./public/json/options.json';
+        var filestr={};
+        if(0==type){
+            let url='http://top.zhaopin.com/Home/Top50?id=HotJobTop&city=749&time=2';
+             filestr=getAllOptions(url);
+        }else {
+            if(fs.readFileSync(outputFilePath)){
+                filestr=JSON.parse(fs.readFileSync(outputFilePath));
+            }else {
+                let url='http://top.zhaopin.com/Home/Top50?id=HotJobTop&city=749&time=2';
+                 filestr=getAllOptions(url);
+            }
+        }
         res.send(filestr);
     });
     app.get('/', function (req, res) {
         console.log(1111)
         res.render('index', { title: 'Express' });
     });
+    //爬虫所有行业选项
+    function getAllOptions(url){
+        let html = request('GET', url).getBody().toString();
+        let $ = cheerio.load(html);
+        let opt=[];
+        let outputFilePath='./public/json/options.json';
+        $('#industry_10').children('option').each(function () {
+            let json={};
+            json.val=$(this).val();
+            json.text=$(this).text();
+            opt.push(json);
+        });
+        var filestr={};
+        filestr.name="所有行业选项";
+        filestr.data=opt;
+        fs.writeFileSync(outputFilePath,JSON.stringify(filestr));
+        return filestr;
+    }
 };
