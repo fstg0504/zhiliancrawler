a2679998cbdac4adfff5e7bc9942268abd48ad1c
zhangxueshi <598846720@qq.com>
2017-06-17 19:31:10 +0800

    增加全国请求为解决跨域2

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 'use strict';
-let request = require('sync-request');
+// let request = require('sync-request');
+let request = require('request');
 let cheerio = require('cheerio');
 let fs = require('fs');
 module.exports = function(app) {
@@ -23,6 +24,10 @@ module.exports = function(app) {
     app.get('/', function (req, res) {
         res.render('index', { title: 'Express' });
     });
+    app.get('/getDetailByCity', function (req, res) {
+        getDetailByCity();
+        res.send();
+    });
     //爬虫所有行业选项
     function getAllOptions(url){
         let html = request('GET', url).getBody().toString();
@@ -40,5 +45,41 @@ module.exports = function(app) {
         filestr.data=opt;
         fs.writeFileSync(outputFilePath,JSON.stringify(filestr));
         return filestr;
+    }
+    //城市
+    function getDetailByCity(){
+        var options={
+            url:'http://top.zhaopin.com/Home/_Top50Detail?Length=4',
+            method:"POST",
+            data:{
+                industry_10:'',
+                time_10:2,
+                cityId:489,
+                cityName:'',
+                isAjaxRequest:true,
+                id:'HotJobTop'
+            },
+            headers:{
+                "Access-Control-Allow-Origin":"*",
+                "Access-Control-Allow-Methods":"GET,POST",
+                "Accept":'*/*',
+                "Connection":'keep-alive',
+                "Content-Type":'application/x-www-form-urlencoded',
+                "Host":'top.zhaopin.com',
+                "Origin":'http://top.zhaopin.com',
+                "Referer":'http://top.zhaopin.com/Home/Top50?id=HotJobTop&city=749&time=2',
+                "X-Requested-With":'XMLHttpRequest'
+            }
+        };
+        request(options,function (err, response, body) {
+            if(err){
+                console.log(22);
+                console.log(err);
+            }else{
+                console.log(25);
+                console.log(body);
+            }
+        })
+
     }
 };
